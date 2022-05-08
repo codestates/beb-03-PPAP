02ab226dc607dba1d8eaf33f851fe4ee4ca1363b
espressom <espressom@naver.com>
2022-05-08 20:15:31 +0900

    chore(위유랑): delete

--- ppap_client/holder_client/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const tailwindcss = require("tailwindcss");
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [tailwindcss("./tailwind.config.js")],
-  corePlugins: require("tailwind-rn/unsupported-core-plugins"),
-};
