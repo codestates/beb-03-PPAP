b5f3c6517cc222e3f87375871926802ece894bba
EunsuGoh <dmstn96@gmail.com>
2022-05-11 14:01:49 +0900

    fix(고은수) : adminLogin 상태코드 수정

--- server/routes/adminRoute.ts
+++ server/routes/adminRoute.ts
@@ -3,6 +3,8 @@ import {
   makeVisa,
   verifyPassport,
   adminLogin,
+  getPassportRequests,
+  getVisaRequests,
 } from "../controller/adminController";
 
 import express from "express";
@@ -12,5 +14,7 @@ adminRoute.route("/makePass").post(makePassport);
 adminRoute.route("/veriPass").post(verifyPassport);
 adminRoute.route("/makeVisa").post(makeVisa);
 adminRoute.route("/adminlogin").post(adminLogin);
+adminRoute.route("/passportRequests").get(getPassportRequests);
+adminRoute.route("/visaRequests").get(getVisaRequests);
 
 module.exports.adminRoute = adminRoute;
