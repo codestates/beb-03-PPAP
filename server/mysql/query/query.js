7f1669874abe48c511b89efff015d882b4f193ab
EunsuGoh <dmstn96@gmail.com>
2022-05-12 10:39:35 +0900

    fix(고은수) : getPassPort --> innerJoin으로 변경

--- server/mysql/query/query.js
+++ server/mysql/query/query.js
@@ -1,74 +1,78 @@
-const connection = require('../config/mysql');
+const connection = require("../config/mysql");
 
 module.exports.getAllUser = async function getClientUser(callback) {
-    connection.query('SELECT * from CLIENT_USER', function (err, result) {
-        if (err) callback(err, null);
-        else callback(null, result);
-    });
+  connection.query("SELECT * from CLIENT_USER", function (err, result) {
+    if (err) callback(err, null);
+    else callback(null, result);
+  });
 };
 
 module.exports.getUser = async function getClientUser(
-    findFlag,
-    data,
-    callback
+  findFlag,
+  data,
+  callback
 ) {
-    connection.query(
-        `SELECT * FROM GOVERN_USER_CLIENT WHERE ${findFlag}='${data}'`,
-        function (err, result) {
-            if (err) callback(err, null);
-            else callback(null, result);
-        }
-    );
+  connection.query(
+    `SELECT * FROM GOVERN_USER_CLIENT WHERE ${findFlag}='${data}'`,
+    function (err, result) {
+      if (err) callback(err, null);
+      else callback(null, result);
+    }
+  );
 };
 
 module.exports.checkPassReq = async function checkPassReq(data, callback) {
-    connection.query(
-        `SELECT * FROM GOVERN_FA_PASSPORT WHERE clientId='${data}'`,
-        function (err, result) {
-            if (err) callback(err, null);
-            else callback(null, result);
-        }
-    );
+  connection.query(
+    `SELECT * FROM GOVERN_FA_PASSPORT WHERE clientId='${data}'`,
+    function (err, result) {
+      if (err) callback(err, null);
+      else callback(null, result);
+    }
+  );
 };
 
 module.exports.getAdmin = async function getAdminUser(
-    findFlag,
-    data,
-    callback
+  findFlag,
+  data,
+  callback
 ) {
-    connection.query(
-        `SELECT * FROM GOVERN_USER_ADMIN WHERE ${findFlag}='${data}'`,
-        function (err, result) {
-            if (err) callback(err, null);
-            else callback(null, result);
-        }
-    );
+  connection.query(
+    `SELECT * FROM GOVERN_USER_ADMIN WHERE ${findFlag}='${data}'`,
+    function (err, result) {
+      if (err) callback(err, null);
+      else callback(null, result);
+    }
+  );
 };
 module.exports.getPassport = async function getPassportList(
-    findFlag,
-    data,
-    callback
+  findFlag,
+  data,
+  callback
 ) {
-    connection.query(
-        `SELECT * FROM GOVERN_FA_PASSPORT WHERE ${findFlag}='${data}'`,
-        function (err, result) {
-            if (err) callback(err, null);
-            else callback(null, result);
-        }
-    );
+  connection.query(
+    `SELECT * FROM GOVERN_FA_PASSPORT P
+    INNER JOIN GOVERN_USER_CLIENT C 
+    ON P.clientId = C.id
+    WHERE P.${findFlag}='${data}'`,
+    function (err, result) {
+      if (err) callback(err, null);
+      else callback(null, result);
+    }
+  );
 };
 module.exports.getVisaSurveyList = async function getVisaSurveyList(
-    findFlag,
-    data,
-    callback
+  findFlag,
+  data,
+  callback
 ) {
-    connection.query(
-        `SELECT * FROM GOVERN_FA_VISA_SURVEY WHERE ${findFlag}='${data}'`,
-        function (err, result) {
-            if (err) callback(err, null);
-            else callback(null, result);
-        }
-    );
+  connection.query(
+    `SELECT * FROM GOVERN_FA_VISA_SURVEY
+         WHERE ${findFlag}='${data}'`,
+    function (err, result) {
+      if (err) callback(err, null);
+      else callback(null, result);
+    }
+  );
 };
 
 // module.exports.joinTables = async function joinTables(callback) {
@@ -82,24 +86,24 @@ module.exports.getVisaSurveyList = async function getVisaSurveyList(
 // };
 
 module.exports.requestForm = async function requestForm(reqForm, callback) {
-    this.checkPassReq(reqForm.clientId, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data);
-            if (data.length === 0) {
-                connection.query(
-                    `INSERT INTO GOVERN_FA_PASSPORT (clientId, did, photoURI, successyn) VALUES ('${
-                        reqForm.clientId
-                    }','${reqForm.did}','${reqForm.photoURI}','${0}')`,
-                    function (err, result) {
-                        if (err) callback(err, null);
-                        else callback(null, result);
-                    }
-                );
-            } else {
-                callback(null, null);
-            }
-        }
-    });
+  this.checkPassReq(reqForm.clientId, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+      if (data.length === 0) {
+        connection.query(
+          `INSERT INTO GOVERN_FA_PASSPORT (clientId, did, photoURI, successyn) VALUES ('${
+            reqForm.clientId
+          }','${reqForm.did}','${reqForm.photoURI}','${0}')`,
+          function (err, result) {
+            if (err) callback(err, null);
+            else callback(null, result);
+          }
+        );
+      } else {
+        callback(null, null);
+      }
+    }
+  });
 };
