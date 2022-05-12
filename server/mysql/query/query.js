23d870d458f32d6e84b6f6b648cd613d2372c6b9
AndrewKim330 <rendezvous330@gmail.com>
2022-05-12 15:04:06 +0900

    feat(김태희): Set initial structuer of getPassport function (before adding session)

--- server/mysql/query/query.js
+++ server/mysql/query/query.js
@@ -1,45 +1,45 @@
-const connection = require("../config/mysql");
+const connection = require('../config/mysql');
 
 module.exports.getUser = async function getUser(
-  tableFlag,
-  findFlag,
-  data,
-  callback
+    tableFlag,
+    findFlag,
+    data,
+    callback
 ) {
-  connection.query(
-    `SELECT * FROM ${tableFlag} WHERE ${findFlag}='${data}'`,
-    function (err, result) {
-      if (err) callback(err, null);
-      else callback(null, result);
-    }
-  );
+    connection.query(
+        `SELECT * FROM ${tableFlag} WHERE ${findFlag}='${data}'`,
+        function (err, result) {
+            if (err) callback(err, null);
+            else callback(null, result);
+        }
+    );
 };
 
 module.exports.getPassport = async function getPassportList(
-  findFlag,
-  data,
-  countryCode,
-  callback
+    findFlag,
+    data,
+    countryCode,
+    callback
 ) {
-  connection.query(
-    `SELECT * FROM GOVERN_FA_PASSPORT P
+    connection.query(
+        `SELECT * FROM GOVERN_FA_PASSPORT P
     INNER JOIN GOVERN_USER_CLIENT C 
     ON P.clientId = C.id 
     WHERE C.countryCode = "${countryCode}" AND P.${findFlag}=${data}`,
-    function (err, result) {
-      if (err) callback(err, null);
-      else callback(null, result);
-    }
-  );
+        function (err, result) {
+            if (err) callback(err, null);
+            else callback(null, result);
+        }
+    );
 };
 module.exports.getVisaSurveyList = async function getVisaSurveyList(
-  findFlag,
-  data,
-  countryCode,
-  callback
+    findFlag,
+    data,
+    countryCode,
+    callback
 ) {
-  connection.query(
-    `SELECT * FROM GOVERN_FA_VISA FV
+    connection.query(
+        `SELECT * FROM GOVERN_FA_VISA FV
     INNER JOIN GOVERN_FA_VISA_SURVEY V
     ON V.visa_id = FV.visa_id
     INNER JOIN GOVERN_FA_PASSPORT P
@@ -47,31 +47,34 @@ module.exports.getVisaSurveyList = async function getVisaSurveyList(
     INNER JOIN GOVERN_USER_CLIENT C 
     ON P.clientId = C.id
     WHERE V.${findFlag}='${data}' AND FV.countryCode = "${countryCode}"`,
-    function (err, result) {
-      if (err) callback(err, null);
-      else callback(null, result);
-    }
-  );
+        function (err, result) {
+            if (err) callback(err, null);
+            else callback(null, result);
+        }
+    );
 };
 
 module.exports.requestForm = async function requestForm(reqForm, callback) {
-  this.getUser("GOVERN_FA_PASSPORT", "id", reqForm.clientId, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (data.length === 0) {
-        connection.query(
-          `INSERT INTO GOVERN_FA_PASSPORT (clientId, did, photoURI, successyn) VALUES ('${
-            reqForm.clientId
-          }','${reqForm.did}','${reqForm.photoURI}','${0}')`,
-          function (err, result) {
-            if (err) callback(err, null);
-            else callback(null, result);
-          }
-        );
-      } else {
-        callback(null, null);
-      }
-    }
-  });
+    // find user using id(clientId)
+    this.getUser('GOVERN_FA_PASSPORT', 'id', reqForm.clientId, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (data.length === 0) {
+                // none request -> newly transfer
+                connection.query(
+                    `INSERT INTO GOVERN_FA_PASSPORT (clientId, did, photoURI, successyn) VALUES ('${
+                        reqForm.clientId
+                    }','${reqForm.did}','${reqForm.photoURI}','${0}')`,
+                    function (err, result) {
+                        if (err) callback(err, null);
+                        else callback(null, result);
+                    }
+                );
+            } else {
+                // request already exists
+                callback(null, null);
+            }
+        }
+    });
 };
