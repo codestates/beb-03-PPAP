b0e9abfd099751cc8ff7d5e6dfb96e5d54b7ce78
EunsuGoh <dmstn96@gmail.com>
2022-05-23 17:49:37 +0900

    feat(고은수) : UserrequestVisa ~ing

--- ppap_client/holder_client/src/components/UserRequestVisa.tsx
+++ ppap_client/holder_client/src/components/UserRequestVisa.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { View, StyleSheet, Text, Pressable, Button } from "react-native";
 import styled from "styled-components/native";
 import { images } from "../utils/images";
-import { MainButton } from "../components/MainButton"
+import { MainButton } from "../components/MainButton";
+import axios from "axios";
+import env from "../utils/envFile";
+import { useSelector } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Cover = styled.View`
   background-color: lightgray;
@@ -45,27 +49,50 @@ const Container = styled.Pressable.attrs((props) => ({
     disabled ? theme.lightgray : theme.navy};
 `;
 
-const UserRequestVisa = ({
-  visaRequestData,
-}: {
-  visaRequestData?: object;
-}) => 
-
-{
+const UserRequestVisa = ({ visaRequestData }: { visaRequestData?: object }) => {
+  const userInfo = useSelector((state: any) => state.userReducer).data;
   const getVisaVc = async () => {
     console.log("버튼 클릭");
+    let output = await axios.post(
+      `${env.server}/holder/issueVisaVC`,
+      { visa_survey_id: visaRequestData.visa_survey_id },
+      {
+        headers: { authorization: userInfo.accessToken },
+      },
+    );
+    AsyncStorage.getItem("@visa_jwt_arr").then(async (data) => {
+      if (data === null) {
+        console.log("null");
+        const visaArray = [output.data.data.vcVisaJwt];
+        // console.log(visaArray);
+        AsyncStorage.setItem("@visa_jwt_arr", JSON.stringify(visaArray));
+      } else {
+        console.log("data");
+        let visaArray = JSON.parse(data);
+        visaArray.push(output.data.data.vcVisaJwt);
+        // console.log(visaArray);
+        AsyncStorage.setItem("@visa_jwt_arr", JSON.stringify(visaArray));
+      }
+    });
+
+    // visa VC 저장
+    // const visaArray = ["asfdadfs", "aklsjhdflja"];
+    AsyncStorage.getItem("@visa_jwt_arr").then((json) => {
+      console.log(json);
+    });
   };
+
   return (
-    <Pressable >
+    <Pressable>
       <Cover>
-        <VisaCreationDate>발급 신청일 : {visaRequestData.creation_date}</VisaCreationDate>
-        {
-          visaRequestData.success_yn ==="1"?
-          <Container onPress={getVisaVc} >
-          <Title>비자 발급</Title>
+        <VisaCreationDate>
+          발급 신청일 : {visaRequestData.creation_date}
+        </VisaCreationDate>
+        {visaRequestData.success_yn === "1" ? (
+          <Container onPress={getVisaVc}>
+            <Title>비자 발급</Title>
           </Container>
-        :null
-        }
+        ) : null}
         <Text style={{ color: "#fff" }}></Text>
       </Cover>
     </Pressable>
