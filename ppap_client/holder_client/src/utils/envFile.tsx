6cf530dddb847b21a0ff1ebefd6c022990dcc58d
espressom <espressom@naver.com>
2022-05-26 17:50:46 +0900

    chore(위유랑): 컨트랙트 재배포

--- ppap_client/holder_client/src/utils/envFile.tsx
+++ ppap_client/holder_client/src/utils/envFile.tsx
@@ -1,8 +1,8 @@
 const env = {
   server: "https://ppap2.loca.lt",
   clientServer: "https://ppap.loca.lt",
-  rpcUrl: "http://172.30.1.9:7545",
-  registry: "0x39A11C835287E0Fd5AF41F950700085608dA1963",
+  rpcUrl: "http://172.30.1.2:7545",
+  registry: "0x6f0C40a94EAB350ceFd54e60f0eb9e3D23962530",
   nftRegistry: "0x181E4B3f78c0b2f1b56DA8fA85C5812706d171B2",
 };
 export default env;
