2d1f2b66aa01c33b5fb5d041297680aceca34ea9
EunsuGoh <dmstn96@gmail.com>
2022-05-26 15:11:04 +0900

    backup - rhdmstn

--- ppap_client/holder_client/src/utils/envFile.tsx
+++ ppap_client/holder_client/src/utils/envFile.tsx
@@ -1,8 +1,9 @@
 const env = {
-  server: "https://neat-fireant-3.loca.lt",
-  clientServer: "https://plastic-moose-90.loca.lt",
-  rpcUrl: "HTTP://192.168.1.132:7545",
-  registry: "0x15CD7F5b57718b17eD0CacF3386aAad54C65a234",
+  server: "https://server.loca.lt",
+  clientServer: "https://itchy-panda-44.loca.lt",
+  rpcUrl: "http://192.168.1.132:7545",
+  registry: "0x76C0B3C38F43F8F33a232Bac75b95a7440d1AB60",
+  nftRegistry: "0x181E4B3f78c0b2f1b56DA8fA85C5812706d171B2",
   privateKey:
     "5c63127d94d009dba1d7a0e95fedda90cec8e5b259212228eb660c3584ce0391", // 로그인 시 클라이언트 DID에서 추출한 정보를 저장해 주어야 함(DB에 저장해 두어야 함)
   identifier: "0x1734aeA0c964ed9D37CE960c222c67B15C75BCc8", // 로그인 시 클라이언트 DID에서 추출한 정보를 저장해 주어야 함(DB에 저장해 두어야 함)
